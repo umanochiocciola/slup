901dc5a4705d31872908aa84f3b18a46420d44c3
umanochiocciola <59803009+umanochiocciola@users.noreply.github.com>
2021-08-07 18:09:13 +0200

    Add files via upload

---
+++ Socialist Linux Union Packaging/GenerateDescriptor.py
@@ -0,0 +1,31 @@
+import json
+
+name = input('Package name: ')
+vers = input('Version: ')
+exef = input('Executable file: ')
+
+dipe = []
+print('\nInsert package dependencies, leave empty to stop')
+while 1:
+    dname = input('Name: ')
+    if dname == '':
+        break
+    
+    dver = input('Version: ')
+    if dver == '':
+        break
+
+    dipe.append({"name": dname, "version": dversion})
+
+with open('descriptor', 'w') as f:
+    f.write(json.dumps(
+            {
+                 "name": name,
+                 "version": vers,
+                 "execute": exef,
+                 "dependencies": dipe
+            }
+        ))
+
+
+
